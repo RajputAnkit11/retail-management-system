33203bdb9775dd9cb0d346087f36a8a62851f8bb
RajputAnkit11 <anku8603@gmail.com>
2025-02-23 12:11:52 +0530

    security service added

---
+++ security-service/src/test/java/com/retail/security_service/SecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.security_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
