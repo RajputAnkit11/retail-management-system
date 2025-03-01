846d00a23903e52276d92c13cf055e52186e2220
Megha Singh <ms4302572@gmail.com>
2025-03-01 11:52:06 +0530

    Api gateway implemented

--- order-service/src/main/java/com/retail/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/retail/order_service/controller/OrderController.java
@@ -6,8 +6,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
 
-    @GetMapping(path = "/orders")
+    @GetMapping(path="/v1/orders")
     public String getOrders() {
         return "These are orders";
     }
+    @GetMapping(path="/v1/orders/price")
+    public String getOrderPrice() {
+        return "These are order price";
+    }
+    @GetMapping(path="/v1/orders/details")
+    public String getOrderDetails() {
+        return "These are orders details";
+    }
 }
+//localhost:7005/retail/order/v1/orders
+//localhost:7005/retail/order/v1/orders/price
+//localhost:7005/retail/order/v1/orders/details
+
+//localhost:7001/retail/apis/v1/orders
+//localhost:7001/retail/apis/v1/orders/price
+//localhost:7001/retail/apis/v1/orders/details
+
+//localhost:7001/retail/apis+ v1/orders/price
+
